4a780a9f9029b2fdf1c0cb2a87d622353480f2ef
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2022-09-30 19:33:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+function diceRoll() {
+var randomNumber1 = Math.round(Math.random() * 5) + 1 ;
+var randomNumber2 = Math.round(Math.random() * 5) + 1 ;
+
+document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`)
+document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`)
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
+}
+
+butn.addEventListener("click", diceRoll)
